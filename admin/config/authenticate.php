8ca99ad34feb32f0480b6474e6bffced5abaefda
Brenlify <robles.167967@gmail.com>
2023-05-29 16:07:46 +0800

    with admin side with correct dbms credentials

--- admin/config/authenticate.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(isset($_SESSION['userID'])){
-    $id = $_SESSION['auth_id'];
-} else {
-    $_SESSION['message'] = "Please login first!";
-    header('Location: ' . $home . '/login.php');
-    exit;
-}
-?>
--- admin/config/connection.php
+++ admin/config/connection.php
@@ -1,25 +1,9 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "christines";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $database);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-//  echo "Connected";
-?>
-<?php
 // $servername = "localhost";
-// $username = "u235219407_bren2023";
-// $password = "Brenlify2023";
-// $database = "u235219407_carpool_db";
+// $username = "root";
+// $password = "";
+// $database = "christines";
 
 // // Create connection
 // $conn = new mysqli($servername, $username, $password, $database);
@@ -28,6 +12,22 @@ if ($conn->connect_error) {
 // if ($conn->connect_error) {
 //   die("Connection failed: " . $conn->connect_error);
 // }
+
+//  echo "Connected";
+?>
+<?php
+$servername = "localhost";
+$username = "u235219407_christines";
+$password = "Christines2023";
+$database = "u235219407_christines";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 
 // echo "Connected";
 ?>

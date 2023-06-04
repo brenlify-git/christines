74ee8730527a74f6e532942508aef311ecd2af70
Brenlify <robles.167967@gmail.com>
2023-06-04 22:36:09 +0800

    wehb

--- admin/config/connection.php
+++ admin/config/connection.php
@@ -1,25 +1,9 @@
 <?php
 
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $database = "christines";
-
-// // Create connection
-// $conn = new mysqli($servername, $username, $password, $database);
-
-// // Check connection
-// if ($conn->connect_error) {
-//   die("Connection failed: " . $conn->connect_error);
-// }
-
-
-?>
-<?php
 $servername = "localhost";
-$username = "u235219407_christines";
-$password = "Christines2023";
-$database = "u235219407_christines";
+$username = "root";
+$password = "";
+$database = "christines";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
@@ -29,6 +13,23 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
+//  echo "Connected";
+?>
+<?php
+// $servername = "localhost";
+// $username = "u235219407_christines";
+// $password = "Christines2023";
+// $database = "u235219407_christines";
+
+// // Create connection
+// $conn = new mysqli($servername, $username, $password, $database);
+
+// // Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+
+// echo "Connected";
 ?>
 
 

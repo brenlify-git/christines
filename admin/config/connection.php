fcdc8f179d08c59edfa9958761bf2398f79a1961
Brenlify <robles.167967@gmail.com>
2023-06-04 22:20:31 +0800

    restore header cp

--- admin/config/connection.php
+++ admin/config/connection.php
@@ -1,25 +1,9 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "christines";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $database);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-//  echo "Connected";
-?>
-<?php
 // $servername = "localhost";
-// $username = "u235219407_christines";
-// $password = "Christines2023";
-// $database = "u235219407_christines";
+// $username = "root";
+// $password = "";
+// $database = "christines";
 
 // // Create connection
 // $conn = new mysqli($servername, $username, $password, $database);
@@ -29,7 +13,22 @@ if ($conn->connect_error) {
 //   die("Connection failed: " . $conn->connect_error);
 // }
 
-// echo "Connected";
+
+?>
+<?php
+$servername = "localhost";
+$username = "u235219407_christines";
+$password = "Christines2023";
+$database = "u235219407_christines";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
 ?>
 
 
